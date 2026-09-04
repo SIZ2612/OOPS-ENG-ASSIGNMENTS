619080601537011d928f4b32429494e34e69b170
SIZ2612 <shreyazaware2025.etc@mmcoe.edu.in>
2026-09-04 12:30:24 +0530

    Add Pascal's triangle generation in main.cpp
    Implement Pascal's triangle printing based on user input.

--- assignment-01/main.cpp
+++ assignment-01/main.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    int n, value;
+
+    cout << "Enter number of rows: ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        value = 1;
+
+        for (int space = 0; space < n - i; space++)
+            cout << " ";
+
+        for (int j = 0; j <= i; j++)
+        {
+            cout << value << " ";
+            value = value * (i - j) / (j + 1);
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
