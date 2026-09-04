f82faa4249b9e569146d1354b11232ac121ea2e2
SIZ2612 <shreyazaware2025.etc@mmcoe.edu.in>
2026-09-04 12:42:40 +0530

    Update main.cpp

--- assignment-02/main.cpp
+++ assignment-02/main.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int sum = 0, count = 0;
+
+    for (int i = 1; count < 50; i++) {
+        if (i % 2 == 0) {
+            sum = sum + i;
+            count++;
+        }
+    }
+
+    cout << "Sum of the first 50 even numbers = " << sum;
+
+    return 0;
+}
 
